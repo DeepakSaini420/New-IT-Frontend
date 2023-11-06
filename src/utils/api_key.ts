f5e534bc73c7aeb5659517d151d9d39ef47238b4
Deepak Saini <sainideeapk40395@gmail.com>
2023-11-06 19:50:12 +0530

    Fixed some amplify issue

--- src/utils/api_key.ts
+++ src/utils/api_key.ts
@@ -1,2 +1,2 @@
-export const publicKey = '8224f7599f53002ad8a724f6c68b3942';
-export const privateKey = 'a1d3152a99f133c6c99398637afc93e9';
+export const publicKey:string = process.env.EMAIL_PUBLIC_KEY || '';
+export const privateKey:string = process.env.EMAIL_PRIVATE_KEY || '';
